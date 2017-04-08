cfe246d16a028cbec1bf39a1877340f6f697c8b0
Atlante45 <clement.brisset@gmail.com>
2017-04-07 17:08:08 -0700

    Start to setup image library

--- libraries/render-utils/src/RenderDeferredTask.cpp
+++ libraries/render-utils/src/RenderDeferredTask.cpp
@@ -194,7 +194,7 @@ RenderDeferredTask::RenderDeferredTask(RenderFetchCullSortTask::Output items) {
         {
             // Grab a texture map representing the different status icons and assign that to the drawStatsuJob
             auto iconMapPath = PathUtils::resourcesPath() + "icons/statusIconAtlas.svg";
-            auto statusIconMap = DependencyManager::get<TextureCache>()->getImageTexture(iconMapPath, NetworkTexture::STRICT_TEXTURE);
+            auto statusIconMap = DependencyManager::get<TextureCache>()->getImageTexture(iconMapPath, gpu::TextureType::STRICT_TEXTURE);
             addJob<DrawStatus>("DrawStatus", opaques, DrawStatus(statusIconMap));
         }
     }
