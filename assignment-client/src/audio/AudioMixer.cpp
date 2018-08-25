6ae280aadf435d5ca7b1f78a7050bd38dd2c890e
Stephen Birarda <commit@birarda.com>
2018-08-24 18:23:41 -0700

    enumerate a vector of mixable streams for each listener

--- assignment-client/src/audio/AudioMixer.cpp
+++ assignment-client/src/audio/AudioMixer.cpp
@@ -196,17 +196,6 @@ const pair<QString, CodecPluginPointer> AudioMixer::negotiateCodec(vector<QStrin
     return make_pair(selectedCodecName, _availableCodecs[selectedCodecName]);
 }
 
-void AudioMixer::handleNodeKilled(SharedNodePointer killedNode) {
-    // enumerate the connected listeners to remove HRTF objects for the disconnected node
-    auto nodeList = DependencyManager::get<NodeList>();
-
-    nodeList->eachNode([&killedNode](const SharedNodePointer& node) {
-        auto clientData = dynamic_cast<AudioMixerClientData*>(node->getLinkedData());
-        if (clientData) {
-            clientData->removeNode(killedNode->getLocalID());
-        }
-    });
-}
 
 void AudioMixer::handleNodeMuteRequestPacket(QSharedPointer<ReceivedMessage> packet, SharedNodePointer sendingNode) {
     auto nodeList = DependencyManager::get<NodeList>();
@@ -225,32 +214,31 @@ void AudioMixer::handleNodeMuteRequestPacket(QSharedPointer<ReceivedMessage> pac
     }
 }
 
+void AudioMixer::handleNodeKilled(SharedNodePointer killedNode) {
+    auto clientData = dynamic_cast<AudioMixerClientData*>(killedNode->getLinkedData());
+    if (clientData) {
+        // stage the removal of all streams from this node, workers handle when preparing mixes for listeners
+        _workerSharedData.removedNodes.emplace_back(killedNode->getLocalID());
+    }
+}
+
 void AudioMixer::handleKillAvatarPacket(QSharedPointer<ReceivedMessage> packet, SharedNodePointer sendingNode) {
     auto clientData = dynamic_cast<AudioMixerClientData*>(sendingNode->getLinkedData());
     if (clientData) {
         clientData->removeAgentAvatarAudioStream();
-        auto nodeList = DependencyManager::get<NodeList>();
-        nodeList->eachNode([sendingNode](const SharedNodePointer& node){
-            auto listenerClientData = dynamic_cast<AudioMixerClientData*>(node->getLinkedData());
-            if (listenerClientData) {
-                listenerClientData->removeHRTFForStream(sendingNode->getLocalID());
-            }
-        });
+
+        // stage a removal of the avatar audio stream from this Agent, workers handle when preparing mixes for listeners
+        _workerSharedData.removedStreams.emplace_back(sendingNode->getUUID(), sendingNode->getLocalID(), QUuid());
     }
 }
 
 void AudioMixer::removeHRTFsForFinishedInjector(const QUuid& streamID) {
     auto injectorClientData = qobject_cast<AudioMixerClientData*>(sender());
-    if (injectorClientData) {
-        // enumerate the connected listeners to remove HRTF objects for the disconnected injector
-        auto nodeList = DependencyManager::get<NodeList>();
 
-        nodeList->eachNode([injectorClientData, &streamID](const SharedNodePointer& node){
-            auto listenerClientData = dynamic_cast<AudioMixerClientData*>(node->getLinkedData());
-            if (listenerClientData) {
-                listenerClientData->removeHRTFForStream(injectorClientData->getNodeLocalID(), streamID);
-            }
-        });
+    if (injectorClientData) {
+        // stage the removal of this stream, workers handle when preparing mixes for listeners
+        _workerSharedData.removedStreams.emplace_back(injectorClientData->getNodeID(), injectorClientData->getNodeLocalID(),
+                                                      streamID);
     }
 }
 
@@ -370,7 +358,6 @@ AudioMixerClientData* AudioMixer::getOrCreateClientData(Node* node) {
     if (!clientData) {
         node->setLinkedData(unique_ptr<NodeData> { new AudioMixerClientData(node->getUUID(), node->getLocalID()) });
         clientData = dynamic_cast<AudioMixerClientData*>(node->getLinkedData());
-        clientData->setNodeLocalID(node->getLocalID());
         connect(clientData, &AudioMixerClientData::injectorStreamFinished, this, &AudioMixer::removeHRTFsForFinishedInjector);
     }
 
